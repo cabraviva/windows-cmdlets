480aee4558997973893ebdb7df66f83dc0ef9e67
Green_Lab <greencoder001@outlook.com>
2022-04-12 00:06:11 +0200

    Run as admin error

--- _install.js
+++ _install.js
@@ -5,9 +5,14 @@ const fs = require('fs')
 
 const CMDLETS_FOLDER = path.join(__dirname, 'cmdlets')
 
-for (const CMDLET of fs.readdirSync(CMDLETS_FOLDER)) {
-    const CMDLET_PATH = path.join(CMDLETS_FOLDER, CMDLET)
-    const SYSTEM32_PATH = path.join('C:', 'Windows', 'System32', CMDLET)
-    
-    fs.copyFileSync(CMDLET_PATH, SYSTEM32_PATH)
+
+try {
+    for (const CMDLET of fs.readdirSync(CMDLETS_FOLDER)) {
+        const CMDLET_PATH = path.join(CMDLETS_FOLDER, CMDLET)
+        const SYSTEM32_PATH = path.join('C:', 'Windows', 'System32', CMDLET)
+        
+        fs.copyFileSync(CMDLET_PATH, SYSTEM32_PATH)
+    }
+} catch (err) {
+    console.log('Did you thought I was joking when I said open cmd as administrator?')
 }
